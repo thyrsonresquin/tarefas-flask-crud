6ccbd38fde72064df969213166eed9e6804457d6
thyrsonresquin <thyrson@outlook.com>
2026-04-02 17:41:19 -0400

    create

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from models.task import Task
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# CRUD (create, read, update and delete - criar, ler, atualizar e deletar) - operações básicas de um banco de dados
 
-@app.route('/about')
-def about():
-    return 'Página Sobre'
+tasks = []
+task_id_counter = 1
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_counter
+    data = request.get_json()
+    new_task = Task(id=task_id_counter, title=data['title'], description=data.get('description', ''))
+    tasks.append(new_task)
+    task_id_counter += 1
+    return jsonify({"message": "Nova tarefa criada com sucesso"}), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
