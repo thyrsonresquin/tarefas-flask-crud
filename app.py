ed94aa1525cc87ba94b06ada2bef9461f6ebc40a
thyrsonresquin <thyrson@outlook.com>
2026-04-07 12:26:21 -0400

    create atualizado

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def create_task():
     new_task = Task(id=task_id_counter, title=data['title'], description=data.get('description', ''))
     task_id_counter += 1
     tasks.append(new_task)
-    return jsonify({"message": "Nova tarefa criada com sucesso"})
+    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
