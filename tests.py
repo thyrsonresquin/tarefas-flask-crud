0a98f7b06c1ec94d36f18051e032a0597d4401be
thyrsonresquin <thyrson@outlook.com>
2026-04-11 16:06:04 -0400

    testes update e delete

--- tests.py
+++ tests.py
@@ -36,3 +36,34 @@ def test_get_task():
         assert data["description"] == "Descrição da tarefa de teste"
     else:
         pytest.skip("Nenhuma tarefa criada para testar")
+
+def test_update_task():
+    if tasks:
+        task_id = tasks[0]
+        payload = {
+            "title": "Tarefa de teste atualizada",
+            "description": "Descrição atualizada da tarefa de teste",
+            "completed": True
+        }
+        response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=payload)
+        assert response.status_code == 200
+        assert response.json()["message"] == "Tarefa atualizada com sucesso"
+    else:
+        pytest.skip("Nenhuma tarefa criada para testar")
+
+    # nova requisição a tarefa específica para verificar se as alterações foram aplicadas
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["completed"] == payload["completed"]
+    assert data["title"] == payload["title"]
+    assert data["description"] == payload["description"]
+
+def test_delete_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        assert response.json()["message"] == "Tarefa deletada com sucesso"
+    else:
+        pytest.skip("Nenhuma tarefa criada para testar")
